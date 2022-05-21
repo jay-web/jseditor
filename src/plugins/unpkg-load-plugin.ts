83833ac5031ce54a3aaaa1fd6b01430a906e7534
jay-web <jay.developer12@gmail.com>
2022-05-21 17:08:57 +0530

    Added plugin onload handler to bundle .css files, also refactor code in unpkg-load-plugin to remove duplicate code

--- src/plugins/unpkg-load-plugin.ts
+++ src/plugins/unpkg-load-plugin.ts
@@ -11,22 +11,53 @@ export const unpkgLoadPlugin = (inputCode: string) => {
     return {
         name: "unpkg-load-plugin",
         setup(build:esbuild.PluginBuild){
+          // * onLoad for index.js file
+          build.onLoad({filter: /(^index\.js$)/}, () => {
+            return {
+              loader: "jsx",
+              contents: inputCode,
+            };
+          });
+
+          // * onLoad to check required package in cache
+        build.onLoad({filter: /.*/}, async (args:any) => {
+           // * Check in cache first, if package data available return it
+           const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+           if(cachedResult){
+             return cachedResult;
+           }
+        });
+
+          // * onLoad for .css files
+          build.onLoad({filter: /.css$/}, async (args:any) => {
+            
+
+            // * If package data is not available in cache then call it and save into cache
+            const {data, request} = await axios.get(args.path);
+            
+            let escaped = data
+             .replace(/\n/g, '')
+             .replace(/"/g, '\\"')
+             .replace(/'/g, "\\'")
+
+            const contents = `
+                const style = document.createElement('style');
+                style.innerText = '${escaped}';
+                document.head.appendChile(style);
+            `;
+
+            const result: esbuild.OnLoadResult = {
+              loader: "jsx",
+              contents: contents,
+              resolveDir: new URL("./", request.responseURL).pathname
+            }
+            await fileCache.setItem(args.path, result);
+            return result;
+
+          })
+
         build.onLoad({ filter: /.*/ }, async (args: any) => {
-            console.log("onLoad", args);
-    
-            if (args.path === "index.js") {
-              return {
-                loader: "jsx",
-                contents: inputCode,
-              };
-            }
-    
-            // * Check in cache first, if package data available return it
-            const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
-            if(cachedResult){
-              return cachedResult;
-            }
-    
+           
             // * If package data is not available in cache then call it and save into cache
     
             const {data, request} = await axios.get(args.path);
